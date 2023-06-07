d0218d705226b44ebaa8ecec5dd03dc7ba50d153
DESKTOP-NOGAGFD\user <wldnjs2284@naver.com>
2023-06-08 00:10:32 +0900

    2023-06-08 / stage Manager

--- Defence_Game/Assets/Assets/Scripts/Spawn_Manager.cs
+++ Defence_Game/Assets/Assets/Scripts/Spawn_Manager.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class Spawn_Manager : MonoBehaviour
 {
-    public GameObject enermy;
+    public GameObject[] enemies;
     public int enermyCount=25;
     // Start is called before the first frame update
+    public int round = 0;
+    public int leftCount = 0;
+    public TextMeshProUGUI leftMob;
     void Start()
     {
         StartCoroutine(spawn());
@@ -21,8 +24,11 @@ public class Spawn_Manager : MonoBehaviour
     {
         for(int i=0;i<enermyCount;i++)
         {
-            Instantiate(enermy, new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), 0), Quaternion.identity);
+            Instantiate(enemies[round], new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), 0), Quaternion.identity);
+            leftCount++;
+            leftMob.text = leftCount.ToString();
             yield return new WaitForSeconds(0.5f);
+
         }
     }
 }
