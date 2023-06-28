978cf8526779f6405408d68c5445617f5d3e74d8
pgs2285 <wldnjs2284@naver.com>
2023-06-28 14:40:00 +0900

    2023-06-28 / character별 공격 효과음 추가, 메뉴에 소리 조절 추가

--- Defence_Game/Assets/StopButton.cs
+++ Defence_Game/Assets/StopButton.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class StopButton : MonoBehaviour
 {
     public GameObject StopPanel;
+
     public void StopButtonClick(){
         StopPanel.SetActive(true);
         Time.timeScale = 0;
@@ -15,11 +16,23 @@ public class StopButton : MonoBehaviour
         StopPanel.SetActive(false);
         Time.timeScale = 1;
     }
-    public GameObject SoundController;
-    public GameObject SoundSlider;
+    public GameObject SoundController; // BGM 컨트롤러
+    public GameObject SoundSlider; // BGM 슬라이더
+    public GameObject EffectSlider; // 효과음 슬라이더
+    float saveEffectsSlider = 1;
     void Update(){
         if(SoundSlider.activeSelf)
         SoundController.GetComponent<AudioSource>().volume = SoundSlider.GetComponent<Slider>().value;
+
+        if(EffectSlider.activeSelf){
+            saveEffectsSlider = EffectSlider.GetComponent<Slider>().value;
+        }
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach(GameObject player in players){
+            player.GetComponent<AudioSource>().volume = saveEffectsSlider;
+        }
     }
+
+    
 }
 
