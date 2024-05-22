8bcc33ee8d1377def91dca26503836419115ed23
PARK JIN SOO <77185565+bakjinsoo@users.noreply.github.com>
2024-05-22 18:31:37 +0900

    0522-1

--- Defence_Game/Assets/Assets/Scripts/QuizStart.cs
+++ Defence_Game/Assets/Assets/Scripts/QuizStart.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class QuizStart : MonoBehaviour
 {
     public GameObject quizbackground;
     public GameObject response;
     public GameObject timerpannel;
+    public int stage_count = 0;
+    public bool quiz_stage = true;
+    public TextMeshProUGUI quiz_expression;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +20,14 @@ public class QuizStart : MonoBehaviour
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.A)){
+            if (stage_count == 1)
+            {
+                quiz_expression.text = "AIDS는 예방과 관리가 가능한 만성질환이다.";
+            }
+            else if(stage_count == 2)
+            {
+                quiz_expression.text = "정신 질환은 단지 의지력 부족으로 인해 발생한다.";
+            }
             response.SetActive(false);
             quizbackground.SetActive(true);
             timerpannel.SetActive(false);
@@ -25,11 +36,17 @@ public class QuizStart : MonoBehaviour
             response.SetActive(true);
             quizbackground.SetActive(false);
             timerpannel.SetActive(false);
+            if (quiz_stage)
+            {
+                stage_count++;
+                quiz_stage=false;
+            }
         }
         if(Input.GetKeyDown(KeyCode.D)){
             response.SetActive(false);
             quizbackground.SetActive(false);
             timerpannel.SetActive(true);
+            quiz_stage = true;
         }
     }
 }
